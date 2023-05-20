ed268df17c8c8c638928a0c50147bee842449b0e
BrianS-TICS <briansanchezaguilar@gmail.com>
2023-05-20 13:05:24 -0700

    Se agregan rutas, se modifican animaciones, se agregan templates

--- 02-adminpro/src/app/pages/notpagefound/notpagefound.component.ts
+++ 02-adminpro/src/app/pages/notpagefound/notpagefound.component.ts
@@ -3,11 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-notpagefound',
   templateUrl: './notpagefound.component.html',
-  styleUrls: []
+  styleUrls: ['./nopagefound.component.css']
 })
 export class NotpagefoundComponent implements OnInit {
 
   constructor() { }
+
+  public year = new Date().getFullYear();
 
   ngOnInit(): void {
   }
