bc6ab406aef4c8b593bb0785fff9fe7f04d4e4ca
BrianS-TICS <briansanchezaguilar@gmail.com>
2023-05-23 15:56:42 -0700

    Se agrega barra de progreso y controlador genérico

---
+++ 02-adminpro/src/app/components/incrementador/incrementador.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-incrementador',
+  templateUrl: './incrementador.component.html',
+  styleUrls: ['./incrementador.component.css']
+})
+export class IncrementadorComponent {
+
+  progreso: number = 50;
+
+
+  constructor() { }
+
+  get getProcentaje() {
+    return `${this.progreso}%`;
+  }
+
+  cambiarValor(valor: number) {
+
+    if (this.progreso >= 100 && valor >= 0) {
+      this.progreso = 100;
+      return;
+    }
+
+    if (this.progreso <= 0 && valor < 0) {
+      this.progreso = 0;
+      return;
+    }
+
+    this.progreso = this.progreso + valor;
+  }
+
+}
--- 02-adminpro/src/app/pages/pages.module.ts
+++ 02-adminpro/src/app/pages/pages.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { SharedModule } from '../shared/shared.module';
 
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -24,9 +26,11 @@ import { PagesComponent } from './pages/pages.component';
     PagesComponent
   ],
   imports: [
+    FormsModule,
     CommonModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    ComponentsModule
   ]
 })
 export class PagesModule { }
