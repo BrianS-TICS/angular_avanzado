90751507f15688b6ab25b068f3aa8153fc8038e3
BrianS-TICS <briansanchezaguilar@gmail.com>
2023-05-23 12:47:10 -0700

    Se modifica la administracion de las rutas

--- 02-adminpro/src/app/pages/pages.routing.ts
+++ 02-adminpro/src/app/pages/pages.routing.ts
@@ -8,13 +8,12 @@ import { GraficaOneComponent } from './grafica-one/grafica-one.component';
 
 const routes: Routes = [
     {
-        path: '',
+        path: 'dashboard',
         component: PagesComponent,
         children: [
-            { path: 'dashboard', component: DashboardComponent },
+            { path: '', component: DashboardComponent },
             { path: 'progress', component: ProgressComponent },
             { path: 'graphic-one', component: GraficaOneComponent },
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
         ]
     }
 ];
